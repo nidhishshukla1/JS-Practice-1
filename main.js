18626f9457dfec9488852b1fdf63bc88868a5f33
Nidhish kumar Shukla <nidhishshukla1@gmail.com>
2023-09-22 00:28:22 +0530

    Version 2 Update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const buttons = document.querySelectorAll('.button')
 
 buttons.forEach(function(button){
     button.addEventListener('click', function(e){
-        //console.log(e)
+        console.log(e)
         if(e.target.id == "grey") document.querySelector('body').style.backgroundColor = e.target.id;
         if(e.target.id == "blue") document.querySelector('body').style.backgroundColor = e.target.id;
         if(e.target.id == "white") document.querySelector('body').style.backgroundColor = e.target.id;
